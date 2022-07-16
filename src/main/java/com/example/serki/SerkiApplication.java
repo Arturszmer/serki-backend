5400376dbee07ade47d38016a6a08e3b22db3fa7
ArturSzmer <arturszmer@gmail.com>
2022-07-16 10:51:41 +0200

    first commit

---
+++ src/main/java/com/example/serki/SerkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.serki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serki/SerkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
