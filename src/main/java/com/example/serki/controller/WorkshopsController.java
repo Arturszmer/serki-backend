764a9f19850e4f00a4a561617a1eef3fd1545e4b
karolina-rf <karolajn1007@gmail.com>
2022-08-27 12:09:45 +0200

    url refactor

--- src/main/java/com/example/serki/controller/WorkshopsController.java
+++ src/main/java/com/example/serki/controller/WorkshopsController.java
@@ -1,5 +1,7 @@
 package com.example.serki.controller;
 
+
+
 import com.example.serki.DTO.*;
 import com.example.serki.Exceptions.NameAlreadyExistException;
 import com.example.serki.Exceptions.SubCatNotExist;
@@ -10,88 +12,86 @@ import com.example.serki.service.TypeOfTrainingService;
 import com.example.serki.service.WorkshopsService;
 import org.springframework.web.bind.annotation.*;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("workshopsLayout")
+@RequestMapping("/")
 public class WorkshopsController {
     private final WorkshopsService workshopsService;
     private final SubCatService subCatService;
     private final TypeOfTrainingService typeOfTrainingService;
-    private final TrainerService trainerService;
-    private final Mapper mapper;
+
+
 
     public WorkshopsController(
             WorkshopsService workshopsService,
             SubCatService subCatService,
-            TypeOfTrainingService typeOfTrainingService,
-            TrainerService trainerService,
-            Mapper mapper) {
+            TypeOfTrainingService typeOfTrainingService) {
         this.workshopsService = workshopsService;
         this.subCatService = subCatService;
         this.typeOfTrainingService = typeOfTrainingService;
-        this.trainerService = trainerService;
-        this.mapper = mapper;
     }
 
-    @GetMapping("/show")
+
+
+    @GetMapping("/workshops")
     @ResponseBody
     public List<WorkshopsDTO> getWorkShops(){
         return workshopsService.workshopsList();
     }
 
-    @PostMapping("/add")
+
+
+    @PostMapping("/workshops")
     @ResponseBody
     public WorkshopsDTO createWorkshop(@RequestBody WorkshopsDTO workshopsDTO){
         WorkshopsDTO created = workshopsService.addWorkshop(workshopsDTO);
-        created.setList(getAllWorkshopsSubCat());
         return created;
     }
 
-    @GetMapping("/workshopsSubCat")
-    @ResponseBody
-    public List<SubCatDTO> getAllWorkshopsSubCat(){
-        return subCatService.workshopsSubCathegoriesList();
-    }
-    @GetMapping("/workshopsSubCat/{workshopName}")
+
+
+    @GetMapping("workshops/{workshopName}/SubCat")
     @ResponseBody
     public List<SubCatDTO> getWorkshopsSubCat(@PathVariable String workshopName) {
         return workshopsService.getSubCatDTOS(workshopName);
     }
 
-    @PostMapping("/workshopsSubCat/add/{workshopName}")
+
+
+    @PostMapping("/workshop/{workshopName}/SubCat")
     public SubCatDTO createSubWorkshops(@RequestBody SubCatDTO subCatDTO, @PathVariable String workshopName) throws NameAlreadyExistException, WorkshopsNotExistException {
         return subCatService.addWorkshopSubCat(subCatDTO, workshopName);
     }
 
-    @GetMapping("/typeOfTrainings/show")
+
+
+    @GetMapping("/workshop/{workshopName}/SubCat/{SubCatName}/typesOfTrainings")
     @ResponseBody
     public List<TypeOfTrainingDTO> getTypeOfTrainings(){
         return typeOfTrainingService.typeOfTrainings();
     }
 
-    @GetMapping("/workshopsSubCat/typeOfTraining/{subCatName}")
+
+
+    @GetMapping("/workshop/{workshopName}/SubCat/{SubCatName}/typesOfTrainings")
     @ResponseBody
     public List<TypeOfTrainingDTO> getTypeOfSpecificTrainings(@PathVariable String subCatName){
         return subCatService.getTypeOfTrainingDTOS(subCatName);
     }
 
-    @PostMapping("typeOfTraining/add/{subCatName}")
+
+
+    @PostMapping("/workshop/{workshopName}/SubCat/{SubCatName}/typesOfTrainings")
     public TypeOfTrainingDTO createTypeOfTraining(@RequestBody TypeOfTrainingDTO typeOfTrainingDTO, @PathVariable String subCatName) throws NameAlreadyExistException, SubCatNotExist {
         return typeOfTrainingService.addTypeOfTraining(typeOfTrainingDTO, subCatName);
-    }
-
-    @PostMapping("trainers/add")
-    public TrainerDTO addTrainer(@RequestBody TrainerDTO trainerDTO){
-        return trainerService.addTrainer(trainerDTO);
-    }
-
-    @GetMapping("trainers")
-    public List<TrainerDTO> showTrainers(){
-        return trainerService.showAllTrainers();
     }
 }
